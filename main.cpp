b47422d8ddb18ce59f18666676f75370b9c157dc
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2024-03-01 12:46:46 +0100

    .

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 using namespace std;
 
+struct Point {
+    double x;
+    double y;
+};
+
 int main() {
 
     int n;
@@ -10,4 +15,26 @@ int main() {
 
     file >> n;
     cout << "Liczba węzłów: " << n << endl;
+
+    Point points[n];
+
+    for (int i = 0 ; i < n ; i++ ) {
+        double xi;
+        double yi;
+
+        file >> xi;
+        file >> yi;
+
+        points[i].x = xi;
+        points[i].y = yi;
+    }
+
+    // wypisywanie punktow zeby sprawdzic czy poprawnie odczytane z pliku
+    for ( int i = 0 ; i < n ; i++) {
+        cout << "f(" << points[i].x << ") = " << points[i].y << endl;
+    }
+
+    double x;
+    cout << "\nPodaj wartosc x: ";
+    cin >> x;
 }
