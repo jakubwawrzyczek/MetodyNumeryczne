1f207d3259d078c016c69caba522ce6d25b8e11a
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2024-03-12 18:59:27 +0100

    .

--- main.cpp
+++ main.cpp
@@ -4,4 +4,35 @@ using namespace std;
 
 int main() {
 
+    int n;
+
+    fstream file("gauss-data.txt");
+
+    file >> n;
+    cout << "Liczba rownan: " << n << endl;
+
+    int **rownania = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        rownania[i] = new int[n];
+    }
+
+    // Wypelniane tablicy wartosciami wspolczynnikow
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int g;
+            file >> g;
+            rownania[i][j] = g;
+        }
+    }
+
+    // Wypisywanie wspolczynnikow przed rownaniami
+    cout << "\nWspolczynniki:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << "|\t" << rownania[i][j] << "\t";
+        }
+        cout << "|";
+        cout << "\n";
+    }
+
 }
