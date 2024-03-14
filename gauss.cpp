de0a5238beca9086ad8271ff147217c9b40de4e4
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2024-03-14 10:03:05 +0100

    refactoring

--- gauss.cpp
+++ gauss.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 using namespace std;
 
-void wypisz_macierz(double** macierz, int n) {
+void print_matrix(double** macierz, int n) {
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n+1; ++j) {
             cout << "|\t" << macierz[i][j] << "\t";
@@ -12,13 +12,13 @@ void wypisz_macierz(double** macierz, int n) {
     }
 }
 
-double mnoznik(int i1, int i2, int j, double** macierz) {
+double multiplier(int i1, int i2, int j, double** macierz) {
 //    cout << macierz[i1][j];
 //    cout << macierz[i2][j];
     return macierz[i1][j]/macierz[i2][j];
 }
                         // wa - wb*mab
-void odejmij_wiersze(int wa, int wb, double mab, int n, double** macierz) {
+void substract_row(int wa, int wb, double mab, int n, double** macierz) {
 
     for (int i = 0; i <= n; ++i) {
         macierz[wa][i] = macierz[wa][i] - macierz[wb][i]*mab;
@@ -26,7 +26,7 @@ void odejmij_wiersze(int wa, int wb, double mab, int n, double** macierz) {
 
 }
 
-void wyzeruj_po_przekatnej(double** macierz, int n) {
+void upper_triangular(double** macierz, int n) {
     // w1 - w0*m10, w2 - w0*m20, w3 - w0*m30
     // w2 - w1*m21, w3 - w1*m31
     // w3 - w2*m32
@@ -39,17 +39,17 @@ void wyzeruj_po_przekatnej(double** macierz, int n) {
 //            cout << "Wiersz od ktorego odejmujemy: " << wa << ", Wiersz ktory odejmujemy: " << wb << endl;
 
             // odejmowanie
-            double mab = mnoznik(wa, wb, wb, macierz);
+            double mab = multiplier(wa, wb, wb, macierz);
 //            cout << mab;
 
-            odejmij_wiersze(wa, wb, mab, n, macierz);
+            substract_row(wa, wb, mab, n, macierz);
         }
 
         wb++;
     }
 }
 
-double wylicz_x(int i, int n, double** macierz, double* x) {
+double compute_x(int i, int n, double** macierz, double* x) {
     double b_i = macierz[i][n];
 
     double a_ii = macierz[i][i];
@@ -101,14 +101,14 @@ int main() {
 
 
     cout << "\n--- Macierz rozszerzona ---" << endl;
-    wypisz_macierz(rownania, n); // przed odejmowaniem wierszy
+    print_matrix(rownania, n); // przed odejmowaniem wierszy
 
 
-    //    cout << mnoznik(2, 1, 1, rownania); // sprawdzenie czy mnoznik jest wyliczany prawidlowo
-    wyzeruj_po_przekatnej(rownania, n);
+    //    cout << multiplier(2, 1, 1, rownania); // sprawdzenie czy mnoznik jest wyliczany prawidlowo
+    upper_triangular(rownania, n);
 
     cout << "\n--- Macierz rozszerzona po przeksztalceniach ---" << endl;
-    wypisz_macierz(rownania, n);    // po odjeciu wierszy
+    print_matrix(rownania, n);    // po odjeciu wierszy
 
 
     // tablica przechowuajca wartosci x
@@ -118,7 +118,7 @@ int main() {
     x[n-1] = rownania[n-1][n]/rownania[n-1][n-1];
 
     for (int i = n-2; i >= 0; --i) {
-        x[i] = wylicz_x(i, n, rownania, x);
+        x[i] = compute_x(i, n, rownania, x);
     }
     
     // wypisywanie x
